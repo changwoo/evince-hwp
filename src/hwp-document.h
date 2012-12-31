9c99c1e1a8c7fc7568f80db75ff05cf922db6ddc
Hodong Kim <cogniti@gmail.com>
2013-01-01 01:15:23 +0900

    code cleanup

--- src/hwp-document.h
+++ src/hwp-document.h
@@ -1,7 +1,6 @@
 /*
  * hwp-document.h
  *
- * Copyright (C) 2010  Carlos Garcia Campos <carlosgc@gnome.org>
  * Copyright (C) 2012  Hodong Kim <cogniti@gmail.com>
  * 
  * hwp-document.h is free software: you can redistribute it and/or modify it
@@ -18,8 +17,8 @@
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef _HWP_DOCUMENT_H_
-#define _HWP_DOCUMENT_H_
+#ifndef __HWP_DOCUMENT_H__
+#define __HWP_DOCUMENT_H__
 
 #include <glib-object.h>
 
@@ -28,15 +27,15 @@
 
 G_BEGIN_DECLS
 
-#define HWP_TYPE_DOCUMENT             (hwp_document_get_type ())
-#define HWP_DOCUMENT(obj)             (G_TYPE_CHECK_INSTANCE_CAST ((obj), HWP_TYPE_DOCUMENT, HWPDocument))
-#define HWP_DOCUMENT_CLASS(klass)     (G_TYPE_CHECK_CLASS_CAST ((klass), HWP_TYPE_DOCUMENT, HWPDocumentClass))
-#define HWP_IS_DOCUMENT(obj)          (G_TYPE_CHECK_INSTANCE_TYPE ((obj), HWP_TYPE_DOCUMENT))
-#define HWP_IS_DOCUMENT_CLASS(klass)  (G_TYPE_CHECK_CLASS_TYPE ((klass), HWP_TYPE_DOCUMENT))
-#define HWP_DOCUMENT_GET_CLASS(obj)   (G_TYPE_INSTANCE_GET_CLASS ((obj), HWP_TYPE_DOCUMENT, HWPDocumentClass))
+#define HWP_TYPE_DOCUMENT            (hwp_document_get_type ())
+#define HWP_DOCUMENT(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), HWP_TYPE_DOCUMENT, HWPDocument))
+#define HWP_DOCUMENT_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass),  HWP_TYPE_DOCUMENT, HWPDocumentClass))
+#define HWP_IS_DOCUMENT(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), HWP_TYPE_DOCUMENT))
+#define HWP_IS_DOCUMENT_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass),  HWP_TYPE_DOCUMENT))
+#define HWP_DOCUMENT_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj),  HWP_TYPE_DOCUMENT, HWPDocumentClass))
 
+typedef struct _HWPDocument      HWPDocument;
 typedef struct _HWPDocumentClass HWPDocumentClass;
-typedef struct _HWPDocument HWPDocument;
 
 GType hwp_document_get_type (void) G_GNUC_CONST;
 
@@ -44,5 +43,4 @@ G_MODULE_EXPORT GType register_evince_backend (GTypeModule *module);
 
 G_END_DECLS
 
-#endif /* _HWP_DOCUMENT_H_ */
-
+#endif /* __HWP_DOCUMENT_H__ */
